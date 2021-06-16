fb53d7358c9ee5a5fce167433111027a52ec60a4
pavel <saltybrain@yandex.ru>
2021-06-16 14:48:09 +0300

    add movies-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { CategoryComponent } from './main-page/category/category.component';
+import { MoviesListComponent } from './main-page/movies-list/movies-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     ToolbarComponent,
-    CategoryComponent
+    CategoryComponent,
+    MoviesListComponent
   ],
   imports: [
     BrowserModule,
