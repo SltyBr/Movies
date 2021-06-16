765e8987e60f628132a4c9a86ead79419d461632
pavel <saltybrain@yandex.ru>
2021-06-16 13:31:37 +0300

    update root files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
