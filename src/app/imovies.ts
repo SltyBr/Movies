2cf8f2e28d42d3ae1dcb45f76e64949f838c6376
pavel <saltybrain@yandex.ru>
2021-06-18 12:24:19 +0300

    modified interfaces

--- src/app/imovies.ts
+++ src/app/imovies.ts
@@ -1,7 +1,8 @@
+import { IDateRange } from "./idate-range";
 import { IMovie } from "./imovie";
 
 export interface IMovies {
-  dates: [],
+  dates: IDateRange[],
   page: number,
   results: IMovie[],
   total_pages: number,
