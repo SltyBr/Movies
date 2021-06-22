766ec76bfc836df3c5cf16cf80d02943d8c29c3b
pavel <saltybrain@yandex.ru>
2021-06-22 19:07:08 +0300

    add queryParams for routing

--- src/app/main-page/components/movies-list/movies-list.component.ts
+++ src/app/main-page/components/movies-list/movies-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { switchMap, takeUntil } from 'rxjs/operators';
 import { IMovie } from 'src/app/main-page/config/models/imovie';
 import { MoviesService } from 'src/app/main-page/services/movies/movies.service';
 
@@ -13,41 +13,48 @@ import { MoviesService } from 'src/app/main-page/services/movies/movies.service'
 export class MoviesListComponent implements OnInit, OnDestroy {
   public movies: IMovie[] = [];
   public totalPages!: number;
-  private onDestroy$ = new Subject<void>();
   public pageNumber: number = 1;
+  private onDestroy$ = new Subject<void>();
 
   constructor( 
-    private moviesService: MoviesService
+    private moviesService: MoviesService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
   ) {}
+  
+  ngOnInit(): void {
+    this.activatedRoute.queryParams
+      .pipe(
+        switchMap(queryParams => {
+          this.pageNumber = queryParams.page ? +queryParams.page : 1;
+          return this.moviesService.getMovies(queryParams.page);
+        }),
+        takeUntil(this.onDestroy$))
+      .subscribe(moviesData => {
+        this.movies = moviesData.results;
+        this.totalPages = moviesData.total_pages;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
+  }
 
   getMoviePosterPath(moviePosterPath: string): string {
     return `http://image.tmdb.org/t/p/w342${moviePosterPath}`;
   }
 
   getMoviesService(page: number){
-    this.moviesService
-        .getMovies(page)
-        .pipe(takeUntil(this.onDestroy$))
-        .subscribe( (moviesData)=>{
-          this.movies = moviesData.results;
-        });
+    this.router.navigate(['/movies'], {
+      queryParams: {
+        page
+      },
+      replaceUrl: true
+    })
   }
 
   trackByMovieId(index: number, movie: IMovie): number{
     return movie.id;
-  }
-  
-  ngOnInit(): void {
-    this.getMoviesService(this.pageNumber);
-    this.moviesService.getMovies(this.pageNumber)
-    .pipe(takeUntil(this.onDestroy$))
-    .subscribe( (moviesData)=>{
-      this.totalPages = moviesData.total_pages;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.onDestroy$.next();
-    this.onDestroy$.complete();
   }
 }
