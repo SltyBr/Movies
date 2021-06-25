19e2d609b1b599b4091e6e5bad226078be8cb868
SaltyBr <SaltyBrain@yandex.ru>
2021-06-25 21:34:23 +0300

    add template style and func for movie item component

--- src/app/main-page/components/movies-item/movies-item.component.ts
+++ src/app/main-page/components/movies-item/movies-item.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IMovie } from '../../config/models/imovie';
+import { MoviesService } from '../../services/movies/movies.service';
 
 @Component({
   selector: 'app-movies-item',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesItemComponent implements OnInit {
 
-  constructor() { }
+  movie!: IMovie;
+  title!: string;
+  releaseDate!: string;
+  voteAverage!: number;
+  popularity!: number;
+  overview!: string;
+  posterPath!: string;
+  backdropPath!: string;
+
+  red = 'red';
+
+  constructor(
+    private moviesService: MoviesService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.getMovieById()
+  }
+
+  getActivatedRouter(): number{
+    return this.activatedRoute.snapshot.params.id;
+  }
+  
+  getMovieById(): void{
+    this.moviesService.getMovieItem(this.getActivatedRouter())
+        .subscribe(movie => {
+          this.movie = movie;
+          this.title = this.movie.title;
+          this.releaseDate = this.movie.release_date;
+          this.voteAverage = this.movie.vote_average;
+          this.popularity = this.movie.popularity;
+          this.overview = this.movie.overview;
+          this.posterPath = this.movie.poster_path;
+          this.backdropPath = this.movie.backdrop_path;});
+  }
+
+  getMoviePoster(moviePosterPath: string): string {
+    return `http://image.tmdb.org/t/p/w342${moviePosterPath}`;
+  }
+  getMovieImageUrl(moviePath: string): string {
+    return `url(http://image.tmdb.org/t/p/w342${moviePath})`;
   }
 
 }
